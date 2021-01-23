59128b0aa87c1459e2e848935a35414c69c7ed0e
sjash <sjash@Jashandeep>
2021-01-22 20:00:08 -0500

    Json ignore properties used to ignore class related objects.

--- webProject/src/com/webProject/bean/Threads.java
+++ webProject/src/com/webProject/bean/Threads.java
@@ -14,6 +14,11 @@ import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -32,6 +37,7 @@ public class Threads {
 	private String threadname;
 	private String threadpost;
 //	@Temporal(value = TemporalType.TIMESTAMP)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.S", timezone = "EST")
 	private Timestamp threadcreated;
 	
 	@ManyToOne(optional = false)
@@ -44,5 +50,6 @@ public class Threads {
 	
 	@OneToMany(mappedBy = "thread", fetch = FetchType.EAGER)
 	@OrderBy("replycreated desc")
+	@JsonIgnoreProperties("thread")
 	private List<Replies> replies;
 }
